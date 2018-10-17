d2b65047c588f8e3c7c8eb4d92d4925b023bfae4
raul <raul@bestjobs.eu>
2018-10-17 22:16:19 +0300

    webpack

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,6 +11,7 @@ namespace App\Controller;
 
 use App\Service\Greeting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -33,18 +34,26 @@ class BlogController extends AbstractController
      * @var SessionInterface
      */
     private $session;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
 
-    public function __construct(Twig_Environment $twig, SessionInterface $session)
+    public function __construct(
+        \Twig_Environment $twig,
+        SessionInterface $session,
+        RouterInterface $router)
     {
         $this->twig = $twig;
         $this->session = $session;
+        $this->router = $router;
     }
 
 
     /**
      * @Route("/", name="blog_index")
      */
-    public function index($name)
+    public function index()
     {
         $html = $this->twig->render('blog/index.html.twig',
             [
@@ -63,9 +72,12 @@ class BlogController extends AbstractController
         $posts[uniqid()] = [
             'title' => 'A random title '.rand(1,500),
             'text' => 'Some random text nr '.rand(1,500),
+            'date' => new \DateTime(),
         ];
 
         $this->session->set('posts', $posts);
+
+        return new RedirectResponse($this->router->generate('blog_index'));
     }
 
     /**
@@ -79,7 +91,7 @@ class BlogController extends AbstractController
             throw new NotFoundHttpException('Post not found');
         }
 
-        return $this->render('blog/index.html.twig',[
+        return $this->render('blog/post.html.twig',[
             'id' => $id,
             'post' => $posts[$id],
         ]);
---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raul
+ * Date: 10/17/18
+ * Time: 7:27 PM
+ */
+
+namespace App\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension implements GlobalsInterface
+{
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    public function __construct(string $locale)
+    {
+
+        $this->locale = $locale;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('price',[$this,'priceFilter'])
+        ];
+    }
+
+    public function getGlobals()
+    {
+        return [
+            'locale' => $this->locale
+        ];
+    }
+
+    public function priceFilter($number)
+    {
+        return '$'.number_format($number,2,'.','.');
+    }
+}
